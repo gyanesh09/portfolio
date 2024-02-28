deb66d62a59a9012ae7189c7d2b4e8e45176e672
Gyanesh <gyanesh@Gyaneshs-MacBook-Air.local>
2024-02-28 09:27:51 +0530

    Adding form validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -21,6 +21,7 @@ import { FormsModule } from '@angular/forms';
     ContactsComponent,
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,28 +1,34 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { addDoc, collection } from 'firebase/firestore';
 import { db } from '../config/firebase.config';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
-export class ContactsComponent {
+export class ContactsComponent{
 
-  formData = { name: '', email: '', message:'' };
+  constructor(private renderer: Renderer2, public el: ElementRef, private fb: FormBuilder) { }
 
-  constructor(private renderer: Renderer2,public el: ElementRef){}
+  formData = { name: '', email: '', message: '' };
+  contactsForm: FormGroup= this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', Validators.required]
+  });;
 
   submitted = false;
 
-  submitFeedback(form: any): void {
-    if (form.valid) {
+  submitFeedback(): void {
+    if (this.contactsForm.valid) {
       this.submitted = true;
+      this.addFirebaseentry();
     }
-    this.addFirebaseentry();
   }
 
-  async addFirebaseentry(){
+  async addFirebaseentry() {
     try {
       const docRef = await addDoc(collection(db, "userMessages"), {
         name: this.formData.name,
@@ -30,14 +36,14 @@ export class ContactsComponent {
         message: this.formData.message
       });
       console.log("Message Saved successfully!", docRef.id);
-      const toastMsg=this.el.nativeElement.querySelector('.toast-msg');
-      setTimeout (()=>{
-        this.renderer.setStyle(toastMsg,'display','flex');
-        setTimeout(()=>{
-          this.renderer.setStyle(toastMsg,'display','none');
-        },5000)
-      },200)
-    } catch (e) { 
+      const toastMsg = this.el.nativeElement.querySelector('.toast-msg');
+      setTimeout(() => {
+        this.renderer.setStyle(toastMsg, 'display', 'flex');
+        setTimeout(() => {
+          this.renderer.setStyle(toastMsg, 'display', 'none');
+        }, 5000)
+      }, 200)
+    } catch (e) {
       console.error("Error saving message: ", e);
     }
   }
